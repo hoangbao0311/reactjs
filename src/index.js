c4273fbcd379a1a4d1240b426ed3bdf1ae6c6328
hoangbaodeptrai <lolcombination@gmail.com>
2023-09-03 22:50:09 +0700

    vercel

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { Context, MyContext } from "./context/Context";
+import { MyContext } from "./context/Context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
